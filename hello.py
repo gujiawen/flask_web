a943be672c99c0d8534642df772cfd0a14b7864f
gjw <gujiawen000@qq.com>
2015-10-08 15:03:16 +0800

    use base.html as templates to save code

--- hello.py
+++ hello.py
@@ -28,9 +28,9 @@ class NameForm(Form):
    submit = SubmitField('Submit')
 
 
-#@app.errorhandler(404)
-#def page_not_found(e):
-#   return render_template('404.html'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+   return render_template('404.html'), 404
 
 
 #@app.errorhandler(500)
